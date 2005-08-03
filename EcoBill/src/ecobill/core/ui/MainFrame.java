5078c969d2f0cd2ee5ae7ca8d31d20881f058d9b
jfuckerweiler <jfuckerweiler>
2005-08-03 15:28:44 +0000

    no message

--- EcoBill/src/ecobill/core/ui/MainFrame.java
+++ EcoBill/src/ecobill/core/ui/MainFrame.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 
 import org.springframework.context.ApplicationContextAware;
@@ -23,7 +24,7 @@ import org.springframework.beans.factory.InitializingBean;
  * Time: 17:43:36
  *
  * @author Roman R&auml;dle
- * @version $Id: MainFrame.java,v 1.8 2005/08/03 15:15:07 jfuckerweiler Exp $
+ * @version $Id: MainFrame.java,v 1.9 2005/08/03 15:28:44 jfuckerweiler Exp $
  * @since EcoBill 1.0
  */
 public class MainFrame extends JFrame implements ApplicationContextAware, InitializingBean  {
@@ -92,14 +93,26 @@ public class MainFrame extends JFrame implements ApplicationContextAware, Initia
         jtab.addTab("Customer", tab2);
         jtab.addTab("Bill", tab3);
 
-        JLabel lab = new JLabel(new ImageIcon("Startbild.jpg"));
-        lab.setToolTipText("Copyright @ JFuckers");
-        lab.setVisible(true);
-        JLabel descrip = new JLabel("Effiziente und Effektive Kunden-, Artikel- und " +
+
+        JLabel lab1 = new JLabel(new ImageIcon("Startbild.jpg"));
+        lab1.setToolTipText("Copyright @ JFuckers");
+        lab1.setVisible(true);
+        JLabel descrip1 = new JLabel("Effiziente und Effektive Kunden-, Artikel- und " +
                 "Rechnungsverwaltung mit Economy Bill Agenda");
        
-        tab0.add(lab, BorderLayout.CENTER);
-        tab0.add(descrip, BorderLayout.SOUTH);
+        tab0.add(lab1, BorderLayout.CENTER);
+        tab0.add(descrip1, BorderLayout.SOUTH);
+
+        JLabel descrip2 = new JLabel("Hier kommt die ArtikelGui rein");
+        tab1.add(descrip2);
+
+        JLabel descrip3 = new JLabel("Hier kommt die KundenGui rein");
+        tab2.add(descrip3);
+
+        JLabel descrip4 = new JLabel("Hier kommt die RechnungsGui bzw. Rechnungen rein");
+        tab3.add(descrip4);
+
+
         this.getContentPane().add(jtab, BorderLayout.CENTER);
     }
 
@@ -114,6 +127,7 @@ public class MainFrame extends JFrame implements ApplicationContextAware, Initia
         // erstellt MenuItem Datei
         JMenu file = new JMenu("File");
         menuBar.add(file);
+        file.setMnemonic(KeyEvent.VK_F);
 
          // erstellt DateiMenü
         file.add("New Product");
@@ -136,6 +150,7 @@ public class MainFrame extends JFrame implements ApplicationContextAware, Initia
         // erstellt MenuItem Bearbeiten
         JMenu edit = new JMenu("Edit");
         menuBar.add(edit);
+        edit.setMnemonic(KeyEvent.VK_E);
 
         // erstellt BearbeitenMenü
         edit.add("Undo");
@@ -150,6 +165,7 @@ public class MainFrame extends JFrame implements ApplicationContextAware, Initia
         // erstellt MenuItem Hilfe
         JMenu help = new JMenu("Help");
         menuBar.add(help);
+        help.setMnemonic(KeyEvent.VK_H);
 
         // erstellt HilfeMenü
         help.add("Help Topics");
@@ -158,13 +174,16 @@ public class MainFrame extends JFrame implements ApplicationContextAware, Initia
         // erstellt MenuItem Sprache
         JMenu language = new JMenu("Language");
         menuBar.add(language);
+        language.setMnemonic(KeyEvent.VK_L);
 
         // erstellt SprachMenü
         ButtonGroup lang = new ButtonGroup();
         JCheckBoxMenuItem german = new JCheckBoxMenuItem("German");
         german.setState(true);
+        german.setMnemonic(KeyEvent.VK_G);
         JCheckBoxMenuItem english = new JCheckBoxMenuItem("English");
         english.setState(false);
+        english.setMnemonic(KeyEvent.VK_E);
         lang.add(german);
         lang.add(english);
         language.add(german);
