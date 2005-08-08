066dce7900b1f30791e5770d10075687164e4462
raedler <raedler>
2005-08-08 18:50:22 +0000

    Internationalisation hinzugefügt

---
+++ EcoBill/src/ecobill/core/system/Internationalization.java
@@ -0,0 +1,18 @@
+package ecobill.core.system;
+
+// @todo document me!
+
+/**
+ * Internationalization.
+ * <p/>
+ * User: rro
+ * Date: 08.08.2005
+ * Time: 19:53:52
+ *
+ * @author Roman R&auml;dle
+ * @version $Id: Internationalization.java,v 1.1 2005/08/08 18:50:22 raedler Exp $
+ * @since EcoBill 1.0
+ */
+public interface Internationalization {
+    public void reinit();
+}
