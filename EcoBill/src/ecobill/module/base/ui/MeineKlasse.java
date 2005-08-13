d91f4298e4e4eb6a534158be87a117a6123d0ed9
raedler <raedler>
2005-08-13 10:48:17 +0000

    example

---
+++ EcoBill/src/ecobill/module/base/ui/MeineKlasse.java
@@ -0,0 +1,36 @@
+package ecobill.module.base.ui;
+
+import javax.swing.*;
+
+// @todo document me!
+
+/**
+ * MeineKlasse.
+ * <p/>
+ * User: rro
+ * Date: 13.08.2005
+ * Time: 12:45:47
+ *
+ * @author Roman R&auml;dle
+ * @version $Id: MeineKlasse.java,v 1.1 2005/08/13 10:48:17 raedler Exp $
+ * @since EcoBill 1.0
+ */
+public class MeineKlasse {
+
+    private JPanel panel;
+
+    public MeineKlasse(JPanel panel) {
+        this.panel = panel;
+    }
+
+    public void tueEtwas() {
+        // do kasch macha.
+        //panel.add(...);
+    }
+
+    public void tueEtwas2(JPanel panel2) {
+        // So kasch au, isch wahrscheinlich besser falls du des panel en dera klasse nur oimol en
+        // dera methode benutzssch.
+        //panel2.add();
+    }
+}
