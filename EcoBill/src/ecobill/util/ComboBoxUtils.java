c148923ed930f0e264a26ada10a893aed411e359
raedler <raedler>
2005-09-28 17:02:31 +0000

    hinzugefügt

---
+++ EcoBill/src/ecobill/util/ComboBoxUtils.java
@@ -0,0 +1,27 @@
+package ecobill.util;
+
+import ecobill.core.util.I18NComboBoxItem;
+
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Roman Georg Rädle
+ * Date: 28.09.2005
+ * Time: 19:00:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComboBoxUtils {
+
+    public static Object[] getI18NTitles() {
+
+        List<Object> l = new LinkedList<Object>();
+
+        l.add(new I18NComboBoxItem("mr"));
+        l.add(new I18NComboBoxItem("mrs"));
+        l.add(new I18NComboBoxItem("firm"));
+
+        return l.toArray();
+    }
+}
