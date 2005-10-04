a9ade7cf09fbb610f4b44084983067f26e7e6c94
raedler <raedler>
2005-10-04 09:32:23 +0000

    export des kompletten schema 2005-10-04

--- EcoBill/export/schema-export.sql
+++ EcoBill/export/schema-export.sql
@@ -1,3 +1,5 @@
+alter table BASE_ARTICLE drop foreign key FK7FD39D68E5FDBA65;
+alter table BASE_ARTICLE drop foreign key FK7FD39D68DAFE8522;
 alter table BASE_ARTICLE_DESCRIPTION drop foreign key FK90003785FBC7C981;
 alter table BASE_BILL drop foreign key FK3B8E70B592A54740;
 alter table BASE_BUSINESS_PARTNER drop foreign key FK2BB6BED7B6A22533;
@@ -5,7 +7,10 @@ alter table BASE_BUSINESS_PARTNER drop foreign key FK2BB6BED733F5BB81;
 alter table BASE_BUSINESS_PARTNER drop foreign key FK2BB6BED7292080C1;
 alter table BASE_DELIVERY_ORDER drop foreign key FK98DB0A5192A54740;
 alter table BASE_DELIVERY_ORDER drop foreign key FK98DB0A51CBBF2233;
+alter table BASE_PERSON drop foreign key FKA96370837F16C806;
+alter table BASE_PERSON drop foreign key FKA96370837C9C9BCE;
 alter table BASE_PERSON drop foreign key FKA9637083292080C1;
+alter table BASE_REDUPLICATED_ARTICLE drop foreign key FK41BAB371FBC7C981;
 alter table BASE_REDUPLICATED_ARTICLE drop foreign key FK41BAB371A0761D98;
 drop table if exists BASE_ADDRESS;
 drop table if exists BASE_ARTICLE;
@@ -16,6 +21,9 @@ drop table if exists BASE_BUSINESS_PARTNER;
 drop table if exists BASE_DELIVERY_ORDER;
 drop table if exists BASE_PERSON;
 drop table if exists BASE_REDUPLICATED_ARTICLE;
+drop table if exists BASE_SYSTEM_COUNTRY;
+drop table if exists BASE_SYSTEM_COUNTY;
+drop table if exists BASE_SYSTEM_LANGUAGE;
 drop table if exists BASE_SYSTEM_LOCALE;
 drop table if exists BASE_SYSTEM_UNIT;
 create table BASE_ADDRESS (
@@ -23,18 +31,18 @@ create table BASE_ADDRESS (
     STREET varchar(255) not null,
     ZIP_CODE varchar(255) not null,
     CITY varchar(255) not null,
-    COUNTY varchar(255) not null,
-    COUNTRY varchar(255) not null,
+    COUNTRY_KEY varchar(255),
+    COUNTY_KEY varchar(255),
     primary key (ID)
 ) type=InnoDB;
 create table BASE_ARTICLE (
     ID bigint not null auto_increment,
     ARTICLE_NUMBER varchar(255) not null unique,
-    SYSTEM_UNIT_KEY varchar(255),
+    UNIT_ID bigint,
     PRICE double precision not null,
     IN_STOCK double precision not null,
     BUNDLE_CAPACITY double precision,
-    BUNDLE_SYSTEM_UNIT_KEY varchar(255),
+    BUNDLE_UNIT_ID bigint,
     primary key (ID)
 ) type=InnoDB;
 create table BASE_ARTICLE_DESCRIPTION (
@@ -60,8 +68,11 @@ create table BASE_BILL (
 ) type=InnoDB;
 create table BASE_BUSINESS_PARTNER (
     ID bigint not null auto_increment,
-    COMPANY_TITLE_KEY varchar(255),
+    CUSTOMER_NUMBER varchar(255) not null unique,
+    COMPANY_TITLE varchar(255),
     COMPANY_NAME varchar(255),
+    COMPANY_BRANCH varchar(255),
+    FOR_ATTENTION_OF bit not null,
     PERSON_ID bigint,
     ADDRESS_ID bigint,
     BANKING_ID bigint,
@@ -70,17 +81,19 @@ create table BASE_BUSINESS_PARTNER (
 create table BASE_DELIVERY_ORDER (
     ID bigint not null auto_increment,
     BUSINESS_PARTNER_ID bigint,
-    DELIVERY_ORDER_NUMBER bigint not null,
+    DELIVERY_ORDER_NUMBER varchar(255) not null unique,
     DELIVERY_ORDER_DATE date not null,
     CHARACTERISATION_TYPE varchar(255) not null,
+    PREFIX_FREE_TEXT text,
+    SUFFIX_FREE_TEXT text,
     PREPARED_BILL bit not null,
     BILL_ID bigint,
     primary key (ID)
 ) type=InnoDB;
 create table BASE_PERSON (
     ID bigint not null auto_increment,
-    TITLE_KEY varchar(255),
-    ACADEMIC_TITLE_KEY varchar(255),
+    TITLE_ID bigint,
+    ACADEMIC_TITLE_ID bigint,
     FIRSTNAME varchar(255),
     LASTNAME varchar(255),
     PHONE varchar(255),
@@ -91,25 +104,56 @@ create table BASE_PERSON (
 ) type=InnoDB;
 create table BASE_REDUPLICATED_ARTICLE (
     ID bigint not null auto_increment,
-    AMOUNT double precision not null,
-    PRICE double precision not null,
-    DESCRIPTION varchar(255) not null,
     DELIVERY_ORDER_ID bigint,
+    ARTICLE_NUMBER varchar(255) not null,
+    AMOUNT double precision not null,
+    UNIT varchar(255) not null,
+    PRICE double precision not null,
+    DESCRIPTION text not null,
+    ARTICLE_ID bigint,
+    primary key (ID)
+) type=InnoDB;
+create table BASE_SYSTEM_COUNTRY (
+    ID bigint not null auto_increment,
+    SYSTEM_COUNTRY_KEY varchar(255) not null unique,
+    SYSTEM_LANGUAGE_KEY varchar(255),
+    primary key (ID)
+) type=InnoDB;
+create table BASE_SYSTEM_COUNTY (
+    ID bigint not null auto_increment,
+    SYSTEM_COUNTY_KEY varchar(255) not null unique,
+    SYSTEM_COUNTRY_KEY varchar(255),
+    primary key (ID)
+) type=InnoDB;
+create table BASE_SYSTEM_LANGUAGE (
+    ID bigint not null auto_increment,
+    SYSTEM_LANGUAGE_KEY varchar(255) not null unique,
     primary key (ID)
 ) type=InnoDB;
 create table BASE_SYSTEM_LOCALE (
     ID bigint not null auto_increment,
     SYSTEM_LOCALE_KEY varchar(255) not null unique,
-    LANGUAGE varchar(255) not null,
-    COUNTRY varchar(255) not null,
+    SYSTEM_LANGUAGE_KEY varchar(255) not null,
+    SYSTEM_COUNTRY_KEY varchar(255) not null,
     VARIANT varchar(255),
     primary key (ID)
 ) type=InnoDB;
 create table BASE_SYSTEM_UNIT (
     ID bigint not null auto_increment,
     UNIT_KEY varchar(255) not null unique,
+    CATEGORY varchar(255) not null,
     primary key (ID)
 ) type=InnoDB;
+alter table BASE_ARTICLE 
+    add index FK7FD39D68E5FDBA65 (BUNDLE_UNIT_ID), 
+    add constraint FK7FD39D68E5FDBA65 
+    foreign key (BUNDLE_UNIT_ID) 
+    references BASE_SYSTEM_UNIT (ID);
+alter table BASE_ARTICLE 
+    add index FK7FD39D68DAFE8522 (UNIT_ID), 
+    add constraint FK7FD39D68DAFE8522 
+    foreign key (UNIT_ID) 
+    references BASE_SYSTEM_UNIT (ID);
 alter table BASE_ARTICLE_DESCRIPTION 
     add index FK90003785FBC7C981 (ARTICLE_ID), 
     add constraint FK90003785FBC7C981 
@@ -146,10 +190,25 @@ alter table BASE_DELIVERY_ORDER
     foreign key (BILL_ID) 
     references BASE_BILL (ID);
 alter table BASE_PERSON 
+    add index FKA96370837F16C806 (ACADEMIC_TITLE_ID), 
+    add constraint FKA96370837F16C806 
+    foreign key (ACADEMIC_TITLE_ID) 
+    references BASE_SYSTEM_UNIT (ID);
+alter table BASE_PERSON 
+    add index FKA96370837C9C9BCE (TITLE_ID), 
+    add constraint FKA96370837C9C9BCE 
+    foreign key (TITLE_ID) 
+    references BASE_SYSTEM_UNIT (ID);
+alter table BASE_PERSON 
     add index FKA9637083292080C1 (ADDRESS_ID), 
     add constraint FKA9637083292080C1 
     foreign key (ADDRESS_ID) 
     references BASE_ADDRESS (ID);
+alter table BASE_REDUPLICATED_ARTICLE 
+    add index FK41BAB371FBC7C981 (ARTICLE_ID), 
+    add constraint FK41BAB371FBC7C981 
+    foreign key (ARTICLE_ID) 
+    references BASE_ARTICLE (ID);
 alter table BASE_REDUPLICATED_ARTICLE 
     add index FK41BAB371A0761D98 (DELIVERY_ORDER_ID), 
     add constraint FK41BAB371A0761D98 
