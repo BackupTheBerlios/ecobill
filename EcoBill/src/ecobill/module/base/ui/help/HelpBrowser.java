dd11f526b56eef78cd6556a1efcd9cd56a52a32b
jfuckerweiler <jfuckerweiler>
2005-10-22 14:49:08 +0000

    no message

--- EcoBill/src/ecobill/module/base/ui/help/HelpBrowser.java
+++ EcoBill/src/ecobill/module/base/ui/help/HelpBrowser.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.io.File;
 import java.net.URL;
+import java.net.MalformedURLException;
 
 // @todo document me!
 
@@ -16,7 +17,7 @@ import java.net.URL;
  * Time: 17:00:54
  *
  * @author Andreas Weiler
- * @version $Id: HelpBrowser.java,v 1.5 2005/10/22 14:39:39 jfuckerweiler Exp $
+ * @version $Id: HelpBrowser.java,v 1.6 2005/10/22 14:49:08 jfuckerweiler Exp $
  * @since EcoBill 1.0
  */
 public class HelpBrowser extends JPanel {
@@ -24,22 +25,24 @@ public class HelpBrowser extends JPanel {
     //private String htmlPath = "file:\\EcoBill\\htmlfiles";
       //  private String page = htmlPath+"index.html";
 
-        private JEditorPane browser = null;
-        private JScrollPane sp = null;
+        private JEditorPane browser = new JEditorPane();
+        private JScrollPane sp = new JScrollPane();
 
-        public HelpBrowser(String page)
-        {
-            browser.setEditable(false);
+
+        public HelpBrowser(String page) throws MalformedURLException {
+
             createBrowser(page);
         }
 
-        public void createBrowser(String page)
-        {
+        public void createBrowser(String page) throws MalformedURLException {
+
             File localFile = new File(page);
 
             try
             {
-               browser = new JEditorPane(new URL(page));
+               localFile.toURL();
+               browser = new JEditorPane(localFile);
+               browser.setEditable(false);
             }
             catch (IOException e)
             {
